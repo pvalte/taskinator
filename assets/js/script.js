e4884a87a1bd8bb6f270fb87dfc73b6eb39517dd
Paige A <pvaltemare@gmail.com>
2021-09-05 14:11:42 -0700

    submit form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,16 @@
 window.onload=function(){
-    var buttonEl = document.querySelector("#save-task"); //button element
+    var formEl = document.querySelector("#task-form"); //form element
     var tasksToDoEl = document.querySelector("#tasks-to-do"); //to do list element
 
-    var createTaskHandler = function() {
+    var createTaskHandler = function(event) {
+        event.preventDefault();
         var listItemEl = document.createElement("li");
         listItemEl.className = "task-item";
         listItemEl.textContent = "This is a new task.";
         tasksToDoEl.appendChild(listItemEl);
     };
 
-    buttonEl.addEventListener("click", createTaskHandler);
+    formEl.addEventListener("submit", createTaskHandler);
 }
 
 
